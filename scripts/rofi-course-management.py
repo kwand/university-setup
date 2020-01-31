26bcc0939f5d849007833c636cca8bf83a5d66db
Daniel Kwan <daniel.w.t.kwan@outlook.com>
2020-01-31 04:12:46 -0500

    Refactored code, changed options into a dict, added Crowdmark/MarkUs functionality

--- scripts/rofi-course-management.py
+++ scripts/rofi-course-management.py
@@ -1,61 +1,74 @@
 #!/usr/bin/python3
 
 from rofi import Rofi
-from courses import Courses
+from courses import Courses, Course
 from config import TERMINAL_EMULATOR, BROWSER
 import subprocess
 
-def open_in_vim(terminal: str, shell: str, file : str):
+def open_in_vim(terminal: str, shell: str, file : str) -> None:
     subprocess.Popen([
         terminal,
         '-e', shell, '-i', '-c',
         "vim " + file
     ])
 
-    return 
-
-def open_in_browser(browser: str, link: str):
+def open_in_browser(browser: str, link: str) -> None:
     subprocess.Popen([browser, link])
+
+def get_options(current : Course) -> dict:
+    options = {
+        'Open course TeX preamble' : 'preamble',
+        'Open TeX snippets': 'snippets', 
+        'Go to the course website': 'website',
+        'Edit YAML info file': 'yaml',
+        'Initialize course directories and files' : 'init'
+    }
+
+    if 'markus' in current.info:
+        options['Open MarkUs'] = 'markus'
+    if 'crowdmark' in current.info:
+        options['Open Crowdmark'] = 'crowdmark'
+
+    return options
+
+def course_action(result : str, current : Course) -> None:
+    if result == 'preamble': # Open course preamble
+        open_in_vim(TERMINAL_EMULATOR, 'fish', f"{str(current.path)}/preamble.tex")
+
+    elif result == 'snippets': # Open snippets
+        open_in_vim(TERMINAL_EMULATOR, 'fish', f"{str(current.path)}/UltiSnips/tex.snippets")
+
+    elif result == 'website': # Open course website
+        open_in_browser(BROWSER, current.info['url'])
+
+    elif result == 'yaml': # Edit YAML info file
+        open_in_vim(TERMINAL_EMULATOR, 'fish', f"{str(current.path)}/info.yaml")
+
+    elif result == 'init': # Initialize course directories
+        (current.path / 'figures').mkdir()
+        (current.path / 'preamble.tex').touch()
+        (current.path / 'UltiSnips').mkdir()
+        (current.path / 'tex.snippets').touch()
+
+    elif result == 'markus': # Open MarkUs
+        open_in_browser(BROWSER, current.info['markus'])
+
+    elif result == 'crowdmark':
+        open_in_browser(BROWSER, current.info['crowdmark'])
 
 
 # Load current course data
 courses = Courses()
-current = courses.current
+current_course = courses.current
 
-options = [
-    'Open course TeX preamble',
-    'Open TeX snippets', 
-    'Go to the course website',
-    'Edit YAML info file',
-    'Initialize course directories and files'
-]
-
-if 'markus' in current.info:
-    options.append('Open MarkUs')
-
+options = get_options(current_course)
+    
 rofi_instance = Rofi(lines=len(options))
 
 index, key = rofi_instance.select(
-    current.info['short'], options
+    current_course.info['short'], list(options)
 )
 
-if index == 0: # Open course preamble
-    open_in_vim(TERMINAL_EMULATOR, 'fish', f"{str(current.path)}/preamble.tex")
-
-elif index == 1: # Open snippets
-    open_in_vim(TERMINAL_EMULATOR, 'fish', f"{str(current.path)}/UltiSnips/tex.snippets")
-
-elif index == 2: # Open course website
-    open_in_browser(BROWSER, current.info['url'])
-
-elif index == 3: # Edit YAML info file
-    open_in_vim(TERMINAL_EMULATOR, 'fish', f"{str(current.path)}/info.yaml")
-
-elif index == 4: # Initialize course directories
-    (current.path / 'figures').mkdir()
-    (current.path / 'preamble.tex').touch()
-    (current.path / 'UltiSnips').mkdir()
-    (current.path / 'tex.snippets').touch()
-
-elif index == 5: # Open MarkUs
-    open_in_browser(BROWSER, current.info['markus'])
+if index >= 0:
+    result = options.get(list(options)[index])
+    course_action(result, current_course)
